f6606314b6a7e3115cd8c7b972e0c55bb7b8832e
matin <elliotmigh@gmail.com>
2024-01-13 08:18:08 +0330

    first commit

---
+++ app/src/main/java/com/example/foodfoodak/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.foodfoodak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}

3d659e1b37b4cabc6671d2844e892335b7a458eb
matin <elliotmigh@gmail.com>
2024-01-13 08:25:50 +0330

    set binding in MainActivity.kt

--- app/src/main/java/com/example/foodfoodak/MainActivity.kt
+++ app/src/main/java/com/example/foodfoodak/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.foodfoodak.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //create binding:
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
